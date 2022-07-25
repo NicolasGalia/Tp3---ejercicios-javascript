d43d9b54695f99860bf39a047c2526264c980251
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-25 10:14:19 -0300

    terminamos el ejercicio numero 3 completo

--- js/Arrays/ejercicio3.js
+++ js/Arrays/ejercicio3.js
@@ -4,14 +4,40 @@ let sumas = [];
 let pruebaDado1 = [];
 let pruebaDado2 = [];
 
-for(let i = 0; i < 50; i++){
-    let dado1 = Math.floor(Math.random()* (6 - 1 + 1) + 1);
-let dado2 = Math.floor(Math.random()* (6 - 1 + 1) + 1);
-let resultado = dado1 + dado2;
-pruebaDado1.push(dado1)
-pruebaDado2.push(dado2)
-sumas.push(resultado);
+for (let i = 0; i < 50; i++) {
+  let dado1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let dado2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let resultado = dado1 + dado2;
+  pruebaDado1.push(dado1);
+  pruebaDado2.push(dado2);
+  sumas.push(resultado);
 }
 document.write("<br>resultado de las sumas: " + sumas);
 document.write("<br>resultado del dado 1: " + pruebaDado1);
 document.write("<br>resultado del dado 2: " + pruebaDado2);
+
+document.write(
+  "<table>" +
+    "<thead>" +
+    " <tr>" +
+    " <th>Resultado</th>" +
+    "  <th>Frecuencia</th>" +
+    "</tr>" +
+    "</thead>" +
+    "<tbody>"
+); 
+for(let i = 2; i <= 12; i++) {
+    document.write(" <tr>"); 
+    let contador = 0;
+    for(let posicion = 0; posicion < sumas.length; posicion++) {
+        if (i === sumas[posicion]){
+            //contar cuantas veces aparecio X volar
+            contador++;
+        }
+    }
+    document.write(
+    "<td>" + i + "</td>" 
+    + "<td>" + contador +"</td>" ); 
+    document.write("</tr>"); 
+}
+document.write("</tbody>" + "</table>");
